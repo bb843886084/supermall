dff6f43ab5dfdce85dc780769c3804d0f1f6514d
bb843886084 <843886084@qq.com>
2023-12-22 11:41:31 +0800

    supermallFirstSubmit

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+</div>
+</template>
+
+<script>
+export default {
+name:"TabBar"
+}
+</script>
+
+<style>
+#tab-bar{
+  display: flex;
+  background-color: #f6f6f6;
+
+  position:fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  box-shadow: 0 -1px 1px rgba(100,100,100,.2);
+}
+
+</style>
+
+
+
+<!-- <div class="tab-bar-item">
+  <img src="../../assets/img/logo.png" alt="">
+  首页</div>
+<div class="tab-bar-item">
+  <img src="../../assets/img/logo.png" alt="">
+  分类</div>
+<div class="tab-bar-item">
+  <img src="../../assets/img/logo.png" alt="">
+  购物车</div>
+<div class="tab-bar-item">
+  <img src="../../assets/img/logo.png" alt="">
+  我的</div> -->
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div>
+      <slot name="item-icon"></slot>
+    </div>
+    <div>
+      <slot name="item-text"></slot>
+    </div>
+
+    <!-- <img src="../../assets/img/logo.png" alt="">
+    <div>首页</div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tabbaritem",
+  props: {
+    path: String,
+  },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path);
+    },
+  },
+};
+</script>
+
+
+
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 3px;
+}
+</style>
